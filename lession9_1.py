fbce9ec2c733f09b594b2797d735bba367475912
remingwang <zqazxws123@gmail.com>
2023-06-04 10:53:04 +0800

    aa

--- lession9_1.py
+++ lession9_1.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
-st.write("### Hello Word")
-st.write("## Hello ***曉明***")
+"### Hello Word"
+"## Hello ***曉明123***"
+
+st.markdown("Hello! **Word**!")
+st.divider()
+st.title("這是APP的title")
+st.divider()
+st.header("這是header")
+st.divider()
+st.subheader("這是subheader")
+st.divider()
+st.caption("這是caption")
+st.divider()
+st.code("a=123")
