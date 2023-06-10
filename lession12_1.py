1e46da6962878fea9edae97d47773ec1e0cc7160
remingwang <77626171+remingwang@users.noreply.github.com>
2023-06-10 06:50:29 +0000

    ST

---
+++ lession12_1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+codeFrame = pd.read_csv('codeSearch.csv',usecols=['code','name'])
+codeSeries = codeFrame['code'].astype(str) + codeFrame['name'] 
+
+
+
+with st.sidebar:
+    st.multiselect("請選擇股票號碼:",codeSeries)
